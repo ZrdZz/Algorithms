2cc1c49bf1c2e100319ba0406acaff4302fa4cb0
zrdzz <17805009970@163.com>
2018-05-14 23:07:24 +0800

    arr

---
+++ 32.MaxSubArr.js
@@ -0,0 +1,11 @@
+//给定一个数组, 找出其中和最大的连续子序列
+
+var maxSubArray = function(nums) {
+    let temp = 0;
+    let res = nums[0];
+    for(let i = 0; i < nums.length; i++){
+        temp = Math.max(nums[i], temp + nums[i]);
+        res = Math.max(temp, res);
+    }
+    return res;
+};
